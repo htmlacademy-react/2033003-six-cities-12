49284e9b53b4d98ddb23a3860c0f0a6ef2dd2f5a
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-03-05 13:25:20 +0300

    выполнил задание по 10 пункт

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+
+type AppScreenProps = {
+  numberRentalOffers: number;
+}
+
+function App({numberRentalOffers}: AppScreenProps): JSX.Element {
+  return (<WelcomeScreen numberRentalOffers={numberRentalOffers}/>);
 }
 
 export default App;
