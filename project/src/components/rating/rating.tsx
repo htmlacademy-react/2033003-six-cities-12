24b6121e5964f2867263bcd0f5a90caf9d0eee60
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-03-16 22:00:31 +0300

    округлил рейтинг но пока не понял почему

--- project/src/components/rating/rating.tsx
+++ project/src/components/rating/rating.tsx
@@ -4,7 +4,11 @@ type RatingProps = {
 
 function Rating({rating}: RatingProps) : JSX.Element {
   const maxRating = 5;
-  const ratingPercentage = (rating / maxRating) * 100;
+  const roundedRating = Math.round(rating);
+  //:TODO если я округляю рейтинг до целого числа тогда для чего width
+  //на странице офера данные разнятся звезд 5 а рейтинг отображается 4.5
+  //в примере структуры данных есть такая строка "rating": 4.8,
+  const ratingPercentage = (roundedRating / maxRating) * 100;
   return (
     <div className="place-card__stars rating__stars">
       <span style={{ width: `${ratingPercentage}%` }}></span>
