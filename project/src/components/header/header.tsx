307038930c616dc008b7364ba76a2887f59b79be
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-03-16 22:46:05 +0300

    добавил задачу

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -28,6 +28,7 @@ function Header({offers}: HeaderProps): JSX.Element {
           <nav className="header__nav">
             <ul className="header__nav-list">
               <li className="header__nav-item user">
+                {/* //TODO:сделать направление на favorites */}
                 <Link className="header__nav-link header__nav-link--profile" to="#">
                   <div className="header__avatar-wrapper user__avatar-wrapper">
                   </div>
