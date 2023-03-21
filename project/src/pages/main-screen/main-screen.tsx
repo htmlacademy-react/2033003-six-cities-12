61adcf6e14e4b6908eb8dd9e84d8ec07f2b525c8
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-03-21 18:55:29 +0300

    исправил ошибки и создал компонент locationList

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import Header from '../../components/header/header';
+import LocationList from '../../components/location/location-list';
 import Map from '../../components/map/map';
 import Offers from '../../components/offers/offers';
 import { Offer } from '../../types/offer';
@@ -15,40 +15,7 @@ function MainScreen({offers} : MainScreenProps) : JSX.Element {
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Paris</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Cologne</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Brussels</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item tabs__item--active" to="#">
-                  <span>Amsterdam</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Hamburg</span>
-                </Link>
-              </li>
-              <li className="locations__item">
-                <Link className="locations__item-link tabs__item" to="#">
-                  <span>Dusseldorf</span>
-                </Link>
-              </li>
-            </ul>
-          </section>
+          <LocationList/>
         </div>
         <div className="cities">
           <div className="cities__places-container container">
