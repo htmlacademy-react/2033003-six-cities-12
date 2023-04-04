dcdb615d44afb7b8adbd8c20752bff5ee490a997
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-04-04 17:30:53 +0300

    подключил redux-thunk

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,4 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { createApi } from '../services/api';
+import { curryGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
 
-export const store = configureStore({reducer: reducer});
+const api = createApi();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
