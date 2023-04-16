2b0177d8086c4f7351c7536e9beb0302d805776c
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-04-16 23:46:24 +0300

    облегчил компонент мэйн скрин

---
+++ project/src/hooks/use-filtered-and-sorted-offers/use-filtered-and-sorted-offers.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '..';
+import { sortOffers } from '../../const';
+import { getOffers } from '../../store/main-data/main-data.selectors';
+import { getLocationName, getSortingMethod } from '../../store/main-process/main-process.selectors';
+import { Offer } from '../../types/offer';
+import { fetchOffersAction } from '../../store/api-actions';
+
+function useFilteredAndSortedOffers(): Offer[] {
+  const dispatch = useAppDispatch();
+  const offers = useAppSelector(getOffers);
+  const selectedCityName = useAppSelector(getLocationName);
+  const selectedSortingMethod = useAppSelector(getSortingMethod);
+
+  useEffect(() => {
+    dispatch(fetchOffersAction());
+  }, [dispatch]);
+
+  return sortOffers(offers, selectedSortingMethod)
+    .filter((offer) => offer.city.name === selectedCityName);
+}
+
+export default useFilteredAndSortedOffers;
--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,35 +1,17 @@
-import { memo, useState } from 'react';
+import { memo, useMemo } from 'react';
 import LocationList from '../../components/location-list/location-list';
 import Map from '../../components/map/map';
-import { useAppSelector } from '../../hooks';
-import { Offer } from '../../types/offer';
-import { AuthorizationStatus, sortOffers } from '../../const';
 import LoadingScreen from '../../components/loading-screen/loading-screen';
-import { getAuthorizationStatus } from '../../store/user-process/user-process.selectors';
-import { getOffers, isOffersDataLoading } from '../../store/main-data/main-data.selectors';
 import OffersEmptyList from '../../components/offers/offers-empty-list';
 import OffersList from '../../components/offers/offers-list';
 import Layout from '../../components/layout/layout';
-import { getLocationName, getSortingMethod } from '../../store/main-process/main-process.selectors';
+import useMainScreen from '../../hooks/use-main-screen/use-main-screen';
 
 function MainScreen() : JSX.Element {
-  const [activeOfferId, setActiveOffer] = useState<number>(-1);
-
-  const offers: Offer[] = useAppSelector(getOffers);
-  const selectedCityName = useAppSelector(getLocationName);
-  const selectedSortingMethod = useAppSelector(getSortingMethod);
-  const authorizationStatus = useAppSelector(getAuthorizationStatus);
-  const isOffersLoading = useAppSelector(isOffersDataLoading);
-
-  const filteredAndSortedOffers: Offer[] = sortOffers(offers,selectedSortingMethod)
-    .filter((offer) => offer.city.name === selectedCityName);
-
-  const city = filteredAndSortedOffers.find((offer) => offer.city.name === selectedCityName)?.city ?? null;
-
-  const isLoading = authorizationStatus === AuthorizationStatus.Unknown || isOffersLoading;
+  const { filteredAndSortedOffers, city, activeOfferId, setActiveOffer, isLoading } = useMainScreen();
+  const MemoizedLocationList = useMemo(() => memo(LocationList), []);
 
   if(!isLoading){
-    const MemoizedLocationList = memo(LocationList);
 
     return(
       <Layout className={`page page--gray page--main ${filteredAndSortedOffers.length === 0 ? 'page--main-empty' : ''}`}>
