be4c2ca6fd90a7561fff042534de484be8823615
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-04-25 19:52:27 +0300

    исправил(2 зачет)

--- project/src/pages/not-found-screen/not-found-screen.test.tsx
+++ project/src/pages/not-found-screen/not-found-screen.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import HistoryRouter from '../../components/history-route/history-route';
+import HistoryRouter from '../../components/history-router/history-router';
 import NotFoundScreen from './not-found-screen';
 import { HelmetProvider } from 'react-helmet-async';
 
